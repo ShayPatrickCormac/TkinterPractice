8d629f6945a4fa33470877ced5ca216b6a7336ab
huangz2 <huangz2@rose-hulman.edu>
2017-10-30 12:26:44 -0400

    m5 done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,7 +2,7 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Zijian Huang.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
@@ -12,37 +12,50 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
 
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
+    Say_hello_button = ttk.Button(frame1, text='Say hello')
+    Say_hello_button.grid()
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
-
+    Say_hello_button['command'] = lambda:print('hello')
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    entry_box = ttk.Entry(frame1)
+    entry_box.grid()
+
+    second_button = ttk.Button(frame1, text='conditional print hello')
+    second_button.grid()
+    second_button['command'] = lambda:a(entry_box)
+
+
+
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -65,10 +78,28 @@ def main():
     #      n = int(s)
     ####################################################################
 
+    second_box = ttk.Entry(frame1)
+    second_box.grid()
+
+    third_button = ttk.Button(frame1,text='multiply')
+    third_button.grid()
+    third_button['command'] = lambda:b(entry_box,second_box)
+
     # ------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
+    root.mainloop()
 
+def a(x):
+    if x.get() == 'ok':
+        print('hello')
+    else:
+        print('Goodbye')
+
+def b(x,y):
+    content = x.get()
+    times = int(y.get())
+    print(content * times)
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
